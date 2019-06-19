870ed90efb3730f74e524cecd52ea6d9587acea1
nsumma147 <nicholas.summa@gmail.com>
2019-06-18 19:14:23 -0500

    decorator practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,17 @@
 import { Component } from '@angular/core';
 
+function log(target, name, descriptor) {
+  console.log(target,name,descriptor)
+  //store the orginal function in a variable
+  const original = descriptor.value
+  //do some manipulations with descriptor.value
+  descriptor.value = function() {
+    console.log("This function was hacked!")
+  }
+  // return the descriptor
+  return descriptor
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +19,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'intro2angular';
+
+  constructor() {
+    this.aSimpleMethod()
+  }
+
+  @log
+  aSimpleMethod() {
+    console.log("Hey there!")
+  }
 }
